9af2f27591c38a36481dae15308ad4d2aee1fb40
Abhaypraja09 <abhaypraja09@gmail.com>
2026-06-18 17:17:24 +0530

    update: fix proxy error for localhost, update api config

--- vite.config.js
+++ vite.config.js
@@ -22,11 +22,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5005',
+        target: 'http://127.0.0.1:5005',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5005',
+        target: 'http://127.0.0.1:5005',
         changeOrigin: true,
       },
     }
